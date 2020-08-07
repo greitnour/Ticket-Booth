ca159a02a4c5dc1869f3a3f0cdbf80f5d8730ef5
greitnour <54603109+greitnour@users.noreply.github.com>
2020-08-06 23:28:08 -0400

    Update TixOrder.java

--- TixOrder.java
+++ TixOrder.java
@@ -1,7 +1,14 @@
+import javax.swing.JOptionPane;
+
 public class TixOrder {
 
-    private String name;
-    private int tixCount;
+    String input = JOptionPane.showInputDialog("Enter a name and a quantity seperated by a colon.
+                                                \n Example: Gabbie:3
+                                                \n Enter STOP to end the program.");
+    String[] array = input.split(":");
+
+    private String name = array[0];
+    private int tixCount = array[1];
 
     public TixOrder (String name, int tixCount){
         this.name = name;
