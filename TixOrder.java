fbec88c5beee7259a16b1c0faf2f9f6e21a1937f
greitnour <54603109+greitnour@users.noreply.github.com>
2020-08-08 13:42:40 -0400

    Update TixOrder.java

--- TixOrder.java
+++ TixOrder.java
@@ -1,14 +1,18 @@
 import javax.swing.JOptionPane;
 
 public class TixOrder {
+    public void Order{
+      String input = JOptionPane.showInputDialog("Enter a name and a quantity seperated by a colon.
+                                                  \n Example: Gabbie:3
+                                                  \n Enter STOP to end the program.");
+      String[] array = input.split(":");
 
-    String input = JOptionPane.showInputDialog("Enter a name and a quantity seperated by a colon.
-                                                \n Example: Gabbie:3
-                                                \n Enter STOP to end the program.");
-    String[] array = input.split(":");
+      String name = array[0];
+      int tixCount = Integer.parseInt(array[1]);
 
-    private String name = array[0];
-    private int tixCount = array[1];
+      //for testing purposes
+      System.out.println(name + " " + tixCount);
+    }
 //Can only get to compile if the remainder of the code is commented out.
 //I'll work on getting that fixed tomorrow.
     public TixOrder (String name, int tixCount){
@@ -23,5 +27,4 @@ public class TixOrder {
     public int getTixCount() {
         return tixCount;
     }
-
 }
